58976ed0ed5a95d923b9689a83c380e14c0b9244
Luilson Sousa <131682396+Luilsons@users.noreply.github.com>
2024-09-22 19:07:05 -0400

    COMP3095- Week 3 - Day2

--- microservice-parent/build.gradle.kts
+++ microservice-parent/build.gradle.kts
@@ -18,8 +18,16 @@ repositories {
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter")
+	implementation("org.springframework.boot:spring-boot-starter-actuator")
+	implementation("org.springframework.boot:spring-boot-starter-data-mongodb")
+	implementation("org.springframework.boot:spring-boot-starter-web")
+	compileOnly("org.projectlombok:lombok")
+	developmentOnly("org.springframework.boot:spring-boot-devtools")
+	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
+	testImplementation("org.springframework.boot:spring-boot-testcontainers")
+	testImplementation("org.testcontainers: junit-jupiter")
+	testImplementation("org.testcontainers: mongodb")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
