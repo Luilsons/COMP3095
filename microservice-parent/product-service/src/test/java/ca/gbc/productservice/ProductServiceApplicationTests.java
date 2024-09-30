de217de3a37fa78f54712d61e33ead11462c694c
Luilson Sousa <131682396+Luilsons@users.noreply.github.com>
2024-09-30 10:21:11 -0400

    COMP3095- WK5 - DAY1

--- microservice-parent/product-service/src/test/java/ca/gbc/productservice/ProductServiceApplicationTests.java
+++ microservice-parent/product-service/src/test/java/ca/gbc/productservice/ProductServiceApplicationTests.java
@@ -1,15 +1,98 @@
 package ca.gbc.productservice;
 
+import io.restassured.RestAssured;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.boot.testcontainers.service.connection.ServiceConnection;
+import org.testcontainers.containers.MongoDBContainer;
 
-@Import(TestcontainersConfiguration.class)
-@SpringBootTest
-class ProductServiceApplicationTests {
+// Tells spring boot to look for a main configuration class (@SpringBootApplication)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class MicroserviceParentApplicationTests {
+
+    // This annotation is used in combination with TestContainers to automatically configure the connection to
+    // the Test MongoDBContainer
+    @ServiceConnection
+    static MongoDBContainer mongoDBContainer = new MongoDBContainer("mongo:latest");
+
+    @LocalServerPort()
+    private Integer port;
+
+    @BeforeEach
+    void setup(){
+        RestAssured.baseURI = "http://localhost:";
+        RestAssured.port = port;
+    }
+
+    static {
+        mongoDBContainer.start();
+    }
 
     @Test
-    void contextLoads() {
+    void createProductTest(){
+
+        String requestBody = """
+				{
+					"Name" : "Samsung TV",
+					"Description : "Samsung TV - Model 2024",
+					"Price : "2000";
+				}
+				""";
+        // BDD - 0 Behavioural Driven Development (Given, When, Then)
+        RestAssured.given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("/products")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .body("id", Matchers.notNullValue())
+                .body("Name", Matchers.equalTo("Samsung TV"))
+                .body("Description", Matchers.equalTo("Samsung TV - Model 2024"))
+                .body("Price", Matchers.equalTo("2000"));
+
+    }
+
+    @Test
+    void getAllProductTest(){
+        String requestBody = """
+				{
+					"Name" : "Samsung TV",
+					"Description : "Samsung TV - Model 2024",
+					"Price : "2000";
+				}
+				""";
+        // BDD - 0 Behavioural Driven Development (Given, When, Then)
+        RestAssured.given()
+                .contentType("application/json")
+                .body(requestBody)
+                .when()
+                .post("/api/products")
+                .then()
+                .log().all()
+                .statusCode(201)
+                .body("id", Matchers.notNullValue())
+                .body("Name", Matchers.equalTo("Samsung TV"))
+                .body("Description", Matchers.equalTo("Samsung TV - Model 2024"))
+                .body("Price", Matchers.equalTo("2000"));
+
+        RestAssured.given()
+                .contentType("application/json")
+                .when()
+                .get("/api/products")
+                .then()
+                .log().all()
+                .statusCode(200)
+                .body("size", Matchers.greaterThan(0))
+                .body("[0].name", Matchers.equalTo("Samsung TV"))
+                .body("Description", Matchers.equalTo("Samsung TV - Model 2024"))
+                .body("Price", Matchers.equalTo("2000"));
+
+
     }
 
 }
